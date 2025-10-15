64c635b49a67dd8a309bf9d8d0ce1f4fd19367e1
Nail-Ui <nailbatuhan1@gmail.com>
2025-10-15 11:51:35 +0300

    V0.2
    Late commit

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+
+    public Color teamColor;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color teamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.teamColor = teamColor;
+
+        string json = JsonUtility.ToJson(data); //https://docs.unity3d.com/ScriptReference/Application-persistentDataPath.html The Unity Scripting API lists the actual paths per platform.
+
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            teamColor = data.teamColor;
+        }
+    }
+}
